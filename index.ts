607a1b2e62dfd3ca9b642c86ab01bc6f07d9b6aa
ilhmwisnu <ilhamwisnu42@gmail.com>
2024-07-03 17:19:19 +0700

    feat: add base route

--- index.ts
+++ index.ts
@@ -8,6 +8,8 @@ app.use(express.json());
 
 app.use("/api",apiRoute);
 
+app.get("/", (req, res) => res.send("Hello World"))
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
