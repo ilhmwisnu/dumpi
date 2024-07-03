e3def0403a1ee4947c80317bd49bfb734fac7dc4
ilhmwisnu <ilhamwisnu42@gmail.com>
2024-07-03 17:11:41 +0700

    refactor: delete unused lines

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import appRoute from "./routes/api";
+import apiRoute from "./routes/api";
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
-app.use("/api",appRoute);
+app.use("/api",apiRoute);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
