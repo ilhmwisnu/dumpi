409f592e317d969fb1cb9d37bf55cd95f4fabd30
Ilham Wisnu <ilhamwisnu42@gmail.com>
2026-05-01 13:23:55 +0700

    feat(users): add POST, PUT, DELETE endpoints (read-only simulation, data not persisted)

--- routes/api/user.ts
+++ routes/api/user.ts
@@ -2,7 +2,11 @@ import { Router } from "express";
 import userController from "../../controller/user";
 
 const userRoute = Router();
+
 userRoute.get("/", userController.getAll);
 userRoute.get("/:id", userController.getById);
+userRoute.post("/", userController.create);
+userRoute.put("/:id", userController.update);
+userRoute.delete("/:id", userController.destroy);
 
 export default userRoute;
